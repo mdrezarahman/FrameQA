fd66734640044e236acce5ae2bee16851b639351
Arsenii Shulikov <a.shulikov@aparavi.com>
2024-08-27 19:26:09 -0700

    feat: added frame processor and some small work on clip processor

---
+++ ai/video_analyzer/frame_processor.py
@@ -0,0 +1,61 @@
+import json
+import os
+import numpy as np
+import ollama
+import cv2
+
+from ..common.config import Config
+
+class FrameProcessor():
+    def __init__(self) -> None:
+        '''
+        Analyze frame information 
+        '''
+        self.start_timestamp = 0.5
+        self.step_timestamp = 2.5
+        self.config = Config.getConfig()
+        self.model = self.config.get('model', "gpt-3.5-turbo")
+        self.prompt = ("Please generate a concise but detailed description for this image. "
+                       "Ensure the description meticulously covers all visible elements. "
+                       "Include details of any text, objects, people, colors, textures, and "
+                       "spatial relationships. Highlight contrasts, interactions, and any "
+                       "notable features that stand out. Avoid assumptions and focus only "
+                       "on what is clearly observable in the image.")
+
+    def process_frames(self, video_clip):
+        result = []
+        t = self.start_timestamp
+        while (t <= video_clip.duration):
+            image = video_clip.make_frame(t)
+            success, encoded_image = cv2.imencode('.png', image)
+            bytes_image = encoded_image.tobytes()
+
+            tmp_result = ollama.generate(
+                model='llava',
+                prompt=self.prompt,
+                images=[bytes_image],
+                stream=False,
+                options={'temperature': 0.0}
+            )['response']
+
+            result.append({
+                "timestamp": t,
+                "text": tmp_result,
+            })
+            t += self.step_timestamp
+        
+        # Mark the timestamp along the text
+        final_text = self.text_post_processing(result)
+
+        return final_text
+
+    def text_post_processing(self, results):
+        prompt = ""
+
+        for result in results:
+            prompt += "frame position: " + str(result["timestamp"]) + " sec\n"
+            prompt += "corresponding text representation of the frame:" + result["text"] + "\n"
+        
+        return prompt
+        
+ 
--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from moviepy.editor import VideoFileClip
 from ai.providers.store.qdrant import Store
 from ai.video_analyzer.audio_processor import AudioProcessor
+from ai.video_analyzer.frame_processor import FrameProcessor
 
 import openai
 
@@ -8,9 +9,17 @@ def load():
     # test the whisper audio extraction process
     video_path = "E:/test_dataset/hackathon_dataset/TU Munich 1.mp4"
     video_clip = VideoFileClip(video_path)
-    obj = AudioProcessor()
-    final_text = obj.process_audio(video_clip=video_clip)
-    print(final_text)
+    audio_proc = AudioProcessor()
+    frame_proc = FrameProcessor()
+    audio_text = audio_proc.process_audio(video_clip=video_clip)
+    frame_text = frame_proc.process_frames(video_clip=video_clip)
+    print(audio_text)
+    print("\n")
+    print("\n")
+    print("=========================================================")
+    print("\n")
+    print("\n")
+    print(frame_text)
 
 
 load()
